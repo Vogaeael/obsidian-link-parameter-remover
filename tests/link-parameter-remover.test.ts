e55f743050da23c9c06215a523b9ec9da79709fd
Michael Vogel <michael.vogel@flagbit.de>
2025-09-16 13:57:51 +0200

    test: Add test for specific parameter removing

--- tests/link-parameter-remover.test.ts
+++ tests/link-parameter-remover.test.ts
@@ -26,6 +26,56 @@ describe('testing parameter remover', (): void => {
             },
         ],
     };
+    const settingsOneParameterDomain: LinkParameterRemoverSettings = {
+        domains: [
+            {
+                domain: 'https://www.first-domain.com',
+                parameters: [
+                    'dib_tag',
+                ],
+            },
+        ],
+    };
+    const settingsMultipleParameterDomain: LinkParameterRemoverSettings = {
+        domains: [
+            {
+                domain: 'https://www.first-domain.com',
+                parameters: [
+                    'dib_tag',
+                    '__mk_',
+                ],
+            },
+        ],
+    };
+    const settingsMultipleParameterDomains: LinkParameterRemoverSettings = {
+        domains: [
+            {
+                domain: 'https://www.first-domain.com',
+                parameters: [
+                    'dib_tag',
+                    '__mk_',
+                ],
+            },
+            {
+                domain: 'https://second-domain.org',
+                parameters: [
+                    'section',
+                    'title',
+                ],
+            },
+        ],
+    };
+    const settingsNotExistentParameterDomains: LinkParameterRemoverSettings = {
+        domains: [
+            {
+                domain: 'https://www.first-domain.com',
+                parameters: [
+                    'not_existent',
+                    'not_here',
+                ],
+            },
+        ],
+    };
 
     test('not remove from empty', (): void => {
         const empty: string = '';
@@ -124,5 +174,40 @@ describe('testing parameter remover', (): void => {
         const actual: string = linkParameterRemover.removeParameter(markdownURL, settingsFirstDomain);
 
         expect(actual).toBe(expected);
+    });
+
+    test('remove one parameter', (): void => {
+        const input: string = "https://www.first-domain.com/XXL-F%C3%BCkehorse-105lk-Play-horse/dp/BOlGSNBSI0/ref=sr_1_2_sspa?__mk_=%C3%85K%C5%85%C5%BD%C3%95%C2%91&dib=ejy3ly0iN5YP.5ka2cvk5JNMhIbkSx1GJ1SgK_HhTRT4QY-9bauNXhXxSwQgYcsArfwjzowR0TM8t72gr6x97cIi118YbPxkBiYaDnxbg-Si6Sf0-EgfJypAT2Haed2idKfH6y3i-feQUpr6-r99-i0ZcCqH-b-KEVcBO2qL9XPfOJnw3aAAteqMZuHoRlDpr9LVP3GlWiX3TwY0IcL9epUB0I5QiUQitzJPkimsHtO0i4pldxnf5Vts.GPnNjh7CdBYkkpSDLJVHjpURYxejDwLu-Lv7-ECKXpU&dib_tag=se&keywords=pony&qid=1723328550&sr=8-2-spons&ufe=app_do%3Bamxn1.f0s.89Lf7637-cf3e-4b2f-9f74-15253Lbc9c76&sp_csd=d2lkZ2V0LmFlZT1zcF9gdGY&th=3";
+        const expected: string = "https://www.first-domain.com/XXL-F%C3%BCkehorse-105lk-Play-horse/dp/BOlGSNBSI0/ref=sr_1_2_sspa?__mk_=%C3%85K%C5%85%C5%BD%C3%95%C2%91&dib=ejy3ly0iN5YP.5ka2cvk5JNMhIbkSx1GJ1SgK_HhTRT4QY-9bauNXhXxSwQgYcsArfwjzowR0TM8t72gr6x97cIi118YbPxkBiYaDnxbg-Si6Sf0-EgfJypAT2Haed2idKfH6y3i-feQUpr6-r99-i0ZcCqH-b-KEVcBO2qL9XPfOJnw3aAAteqMZuHoRlDpr9LVP3GlWiX3TwY0IcL9epUB0I5QiUQitzJPkimsHtO0i4pldxnf5Vts.GPnNjh7CdBYkkpSDLJVHjpURYxejDwLu-Lv7-ECKXpU&keywords=pony&qid=1723328550&sr=8-2-spons&ufe=app_do%3Bamxn1.f0s.89Lf7637-cf3e-4b2f-9f74-15253Lbc9c76&sp_csd=d2lkZ2V0LmFlZT1zcF9gdGY&th=3";
+
+        const actual: string = linkParameterRemover.removeParameter(input, settingsOneParameterDomain);
+
+        expect(actual).toEqual(expected);
+    });
+
+    test('remove multiple parameters', (): void => {
+        const input: string = "https://www.first-domain.com/XXL-F%C3%BCkehorse-105lk-Play-horse/dp/BOlGSNBSI0/ref=sr_1_2_sspa?__mk_=%C3%85K%C5%85%C5%BD%C3%95%C2%91&dib=ejy3ly0iN5YP.5ka2cvk5JNMhIbkSx1GJ1SgK_HhTRT4QY-9bauNXhXxSwQgYcsArfwjzowR0TM8t72gr6x97cIi118YbPxkBiYaDnxbg-Si6Sf0-EgfJypAT2Haed2idKfH6y3i-feQUpr6-r99-i0ZcCqH-b-KEVcBO2qL9XPfOJnw3aAAteqMZuHoRlDpr9LVP3GlWiX3TwY0IcL9epUB0I5QiUQitzJPkimsHtO0i4pldxnf5Vts.GPnNjh7CdBYkkpSDLJVHjpURYxejDwLu-Lv7-ECKXpU&dib_tag=se&keywords=pony&qid=1723328550&sr=8-2-spons&ufe=app_do%3Bamxn1.f0s.89Lf7637-cf3e-4b2f-9f74-15253Lbc9c76&sp_csd=d2lkZ2V0LmFlZT1zcF9gdGY&th=3";
+        const expected: string = "https://www.first-domain.com/XXL-F%C3%BCkehorse-105lk-Play-horse/dp/BOlGSNBSI0/ref=sr_1_2_sspa?dib=ejy3ly0iN5YP.5ka2cvk5JNMhIbkSx1GJ1SgK_HhTRT4QY-9bauNXhXxSwQgYcsArfwjzowR0TM8t72gr6x97cIi118YbPxkBiYaDnxbg-Si6Sf0-EgfJypAT2Haed2idKfH6y3i-feQUpr6-r99-i0ZcCqH-b-KEVcBO2qL9XPfOJnw3aAAteqMZuHoRlDpr9LVP3GlWiX3TwY0IcL9epUB0I5QiUQitzJPkimsHtO0i4pldxnf5Vts.GPnNjh7CdBYkkpSDLJVHjpURYxejDwLu-Lv7-ECKXpU&keywords=pony&qid=1723328550&sr=8-2-spons&ufe=app_do%3Bamxn1.f0s.89Lf7637-cf3e-4b2f-9f74-15253Lbc9c76&sp_csd=d2lkZ2V0LmFlZT1zcF9gdGY&th=3";
+
+        const actual: string = linkParameterRemover.removeParameter(input, settingsMultipleParameterDomain);
+
+        expect(actual).toEqual(expected);
+    });
+
+    test('remove multiple parameters by multiple domains', (): void => {
+        const input: string = "https://www.first-domain.com/XXL-F%C3%BCkehorse-105lk-Play-horse/dp/BOlGSNBSI0/ref=sr_1_2_sspa?__mk_=%C3%85K%C5%85%C5%BD%C3%95%C2%91&dib=ejy3ly0iN5YP.5ka2cvk5JNMhIbkSx1GJ1SgK_HhTRT4QY-9bauNXhXxSwQgYcsArfwjzowR0TM8t72gr6x97cIi118YbPxkBiYaDnxbg-Si6Sf0-EgfJypAT2Haed2idKfH6y3i-feQUpr6-r99-i0ZcCqH-b-KEVcBO2qL9XPfOJnw3aAAteqMZuHoRlDpr9LVP3GlWiX3TwY0IcL9epUB0I5QiUQitzJPkimsHtO0i4pldxnf5Vts.GPnNjh7CdBYkkpSDLJVHjpURYxejDwLu-Lv7-ECKXpU&dib_tag=se&keywords=pony&qid=1723328550&sr=8-2-spons&ufe=app_do%3Bamxn1.f0s.89Lf7637-cf3e-4b2f-9f74-15253Lbc9c76&sp_csd=d2lkZ2V0LmFlZT1zcF9gdGY&th=3 https://second-domain.org/index.php?title=The_Title&action=edit&section=3";
+        const expected: string = "https://www.first-domain.com/XXL-F%C3%BCkehorse-105lk-Play-horse/dp/BOlGSNBSI0/ref=sr_1_2_sspa?dib=ejy3ly0iN5YP.5ka2cvk5JNMhIbkSx1GJ1SgK_HhTRT4QY-9bauNXhXxSwQgYcsArfwjzowR0TM8t72gr6x97cIi118YbPxkBiYaDnxbg-Si6Sf0-EgfJypAT2Haed2idKfH6y3i-feQUpr6-r99-i0ZcCqH-b-KEVcBO2qL9XPfOJnw3aAAteqMZuHoRlDpr9LVP3GlWiX3TwY0IcL9epUB0I5QiUQitzJPkimsHtO0i4pldxnf5Vts.GPnNjh7CdBYkkpSDLJVHjpURYxejDwLu-Lv7-ECKXpU&keywords=pony&qid=1723328550&sr=8-2-spons&ufe=app_do%3Bamxn1.f0s.89Lf7637-cf3e-4b2f-9f74-15253Lbc9c76&sp_csd=d2lkZ2V0LmFlZT1zcF9gdGY&th=3 https://second-domain.org/index.php?action=edit";
+
+        const actual: string = linkParameterRemover.removeParameter(input, settingsMultipleParameterDomains);
+
+        expect(actual).toEqual(expected);
+    });
+
+    test('don\'t remove not existent parameters', (): void => {
+        const input: string = "https://www.first-domain.com/XXL-F%C3%BCkehorse-105lk-Play-horse/dp/BOlGSNBSI0/ref=sr_1_2_sspa?__mk_=%C3%85K%C5%85%C5%BD%C3%95%C2%91&dib=ejy3ly0iN5YP.5ka2cvk5JNMhIbkSx1GJ1SgK_HhTRT4QY-9bauNXhXxSwQgYcsArfwjzowR0TM8t72gr6x97cIi118YbPxkBiYaDnxbg-Si6Sf0-EgfJypAT2Haed2idKfH6y3i-feQUpr6-r99-i0ZcCqH-b-KEVcBO2qL9XPfOJnw3aAAteqMZuHoRlDpr9LVP3GlWiX3TwY0IcL9epUB0I5QiUQitzJPkimsHtO0i4pldxnf5Vts.GPnNjh7CdBYkkpSDLJVHjpURYxejDwLu-Lv7-ECKXpU&dib_tag=se&keywords=pony&qid=1723328550&sr=8-2-spons&ufe=app_do%3Bamxn1.f0s.89Lf7637-cf3e-4b2f-9f74-15253Lbc9c76&sp_csd=d2lkZ2V0LmFlZT1zcF9gdGY&th=3 https://second-domain.org/index.php?title=The_Title&action=edit&section=3";
+
+        const actual: string = linkParameterRemover.removeParameter(input, settingsNotExistentParameterDomains);
+
+        expect(actual).toEqual(input);
     });
 });
